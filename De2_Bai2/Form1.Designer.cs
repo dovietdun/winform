9a41e4d469b34953e41b75d1b61099174c3e6cd1
dovietdun <dovietdun@gmail.com>
2024-01-06 22:12:58 +0700

    winform app

---
+++ De2_Bai2/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace De2_Bai2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_soluong = new System.Windows.Forms.TextBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.soluong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dongia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.thanhtien = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_sohoadon = new System.Windows.Forms.TextBox();
+            this.txt_tenkhachhang = new System.Windows.Forms.TextBox();
+            this.txt_diachi = new System.Windows.Forms.TextBox();
+            this.cbx_khachhang = new System.Windows.Forms.ComboBox();
+            this.txt_date = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txt_date);
+            this.panel1.Controls.Add(this.cbx_khachhang);
+            this.panel1.Controls.Add(this.txt_diachi);
+            this.panel1.Controls.Add(this.txt_tenkhachhang);
+            this.panel1.Controls.Add(this.txt_sohoadon);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(1, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(650, 158);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.soluong,
+            this.dongia,
+            this.thanhtien});
+            this.dataGridView1.Location = new System.Drawing.Point(1, 167);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(650, 299);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(492, 490);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tổng cộng";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_soluong
+            // 
+            this.txt_soluong.Enabled = false;
+            this.txt_soluong.Location = new System.Drawing.Point(557, 487);
+            this.txt_soluong.Name = "txt_soluong";
+            this.txt_soluong.Size = new System.Drawing.Size(94, 20);
+            this.txt_soluong.TabIndex = 3;
+            this.txt_soluong.TextChanged += new System.EventHandler(this.txt_soluong_TextChanged);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Mã hàng";
+            this.Column1.Name = "Column1";
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Tên hàng";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "ĐVT";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // soluong
+            // 
+            this.soluong.HeaderText = "Số lượng";
+            this.soluong.Name = "soluong";
+            // 
+            // dongia
+            // 
+            this.dongia.HeaderText = "Đơn giá";
+            this.dongia.Name = "dongia";
+            // 
+            // thanhtien
+            // 
+            this.thanhtien.HeaderText = "Thành tiền";
+            this.thanhtien.Name = "thanhtien";
+            this.thanhtien.ReadOnly = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(62, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Số hóa đơn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(369, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Ngày";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(62, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Khách hàng";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.Location = new System.Drawing.Point(306, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Tên khách hàng";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label6.Location = new System.Drawing.Point(91, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 16);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Địa chỉ";
+            // 
+            // txt_sohoadon
+            // 
+            this.txt_sohoadon.Location = new System.Drawing.Point(155, 32);
+            this.txt_sohoadon.Name = "txt_sohoadon";
+            this.txt_sohoadon.Size = new System.Drawing.Size(120, 20);
+            this.txt_sohoadon.TabIndex = 5;
+            // 
+            // txt_tenkhachhang
+            // 
+            this.txt_tenkhachhang.Enabled = false;
+            this.txt_tenkhachhang.Location = new System.Drawing.Point(416, 72);
+            this.txt_tenkhachhang.Name = "txt_tenkhachhang";
+            this.txt_tenkhachhang.Size = new System.Drawing.Size(124, 20);
+            this.txt_tenkhachhang.TabIndex = 6;
+            // 
+            // txt_diachi
+            // 
+            this.txt_diachi.Enabled = false;
+            this.txt_diachi.Location = new System.Drawing.Point(155, 108);
+            this.txt_diachi.Name = "txt_diachi";
+            this.txt_diachi.Size = new System.Drawing.Size(385, 20);
+            this.txt_diachi.TabIndex = 7;
+            // 
+            // cbx_khachhang
+            // 
+            this.cbx_khachhang.FormattingEnabled = true;
+            this.cbx_khachhang.Location = new System.Drawing.Point(155, 66);
+            this.cbx_khachhang.Name = "cbx_khachhang";
+            this.cbx_khachhang.Size = new System.Drawing.Size(121, 21);
+            this.cbx_khachhang.TabIndex = 8;
+            this.cbx_khachhang.SelectedValueChanged += new System.EventHandler(this.cbx_khachhang_SelectedValueChanged);
+            // 
+            // txt_date
+            // 
+            this.txt_date.Location = new System.Drawing.Point(416, 32);
+            this.txt_date.Name = "txt_date";
+            this.txt_date.Size = new System.Drawing.Size(124, 20);
+            this.txt_date.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(359, 517);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "&Ghi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(466, 517);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "&Bỏ qua";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 561);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_soluong);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_soluong;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn soluong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dongia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn thanhtien;
+        private System.Windows.Forms.DateTimePicker txt_date;
+        private System.Windows.Forms.ComboBox cbx_khachhang;
+        private System.Windows.Forms.TextBox txt_diachi;
+        private System.Windows.Forms.TextBox txt_tenkhachhang;
+        private System.Windows.Forms.TextBox txt_sohoadon;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ De2_Bai2/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace De2_Bai2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataClasses1DataContext data = new DataClasses1DataContext();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            data = new DataClasses1DataContext();
+            Column1.DataSource = data.tblItemLists;
+            Column1.DisplayMember = "ItemID";
+            Column1.ValueMember = "ItemID";
+            dataGridView1.EditingControlShowing += dataGridView1_EditingControlShowing;
+            //
+            cbx_khachhang.DataSource = data.tblCustomerLists;
+            cbx_khachhang.DisplayMember = "CustomerID";
+            cbx_khachhang.ValueMember = "CustomerID";
+        }
+
+
+
+       
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+        private string GetItemNameByItemID(String itemID)
+        {
+            // Sử dụng DataContext để lấy thông tin từ tblItemLists
+            DataClasses1DataContext data = new DataClasses1DataContext();
+
+            // Tìm đối tượng tblItemList tương ứng với ItemID
+            var selectedItem = data.tblItemLists.SingleOrDefault(item => item.ItemID.Equals(itemID));
+
+            // Kiểm tra nếu đối tượng được tìm thấy
+            if (selectedItem != null)
+            {
+                // Trả về giá trị ItemName
+                return selectedItem.ItemName;
+            }
+
+            // Nếu không tìm thấy, trả về chuỗi trống hoặc giá trị mặc định khác tùy thuộc vào yêu cầu của bạn
+            return string.Empty;
+        }
+        private string GetDVTByItemID(String itemID)
+        {
+            // Sử dụng DataContext để lấy thông tin từ tblItemLists
+            DataClasses1DataContext data = new DataClasses1DataContext();
+
+            // Tìm đối tượng tblItemList tương ứng với ItemID
+            var selectedItem = data.tblItemLists.SingleOrDefault(item => item.ItemID.Equals(itemID));
+
+            // Kiểm tra nếu đối tượng được tìm thấy
+            if (selectedItem != null)
+            {
+                // Trả về giá trị ItemName
+                return selectedItem.InvUnitOfMeasr;
+            }
+
+            // Nếu không tìm thấy, trả về chuỗi trống hoặc giá trị mặc định khác tùy thuộc vào yêu cầu của bạn
+            return string.Empty;
+        }
+        private void CalculateTotalQuantity1()
+        {
+            decimal totalQuantity = 0;
+
+            // Lặp qua tất cả các hàng trong DataGridView và cộng số lượng
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells["soluong"].Value != null)
+                {
+                    decimal soluong;
+                    if (decimal.TryParse(row.Cells["soluong"].Value.ToString(), out soluong))
+                    {
+                        totalQuantity += soluong;
+                    }
+                }
+            }
+
+            // Gán giá trị tổng vào TextBox
+            txt_soluong.Text = totalQuantity.ToString();
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0 && e.RowIndex < dataGridView1.Rows.Count)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+                // Kiểm tra xem hàng có tồn tại không
+                if (row != null)
+                {
+                    DataGridViewCell cell = row.Cells[e.ColumnIndex];
+
+                    // Kiểm tra xem ô cụ thể có tồn tại không
+                    if (cell != null)
+                    {
+                        // Lấy giá trị số lượng và đơn giá từ ô DataGridView
+                        object soluongObj = row.Cells["soluong"].Value;
+                        object dongiaObj = row.Cells["dongia"].Value;
+
+                        // Kiểm tra xem cả hai giá trị đã được nhập đủ chưa
+                        if (soluongObj != null && dongiaObj != null)
+                        {
+                            decimal soluong, dongia;
+
+                            if (decimal.TryParse(soluongObj.ToString(), out soluong) && decimal.TryParse(dongiaObj.ToString(), out dongia))
+                            {
+                                // Tính thành tiền và gán vào cột thanhtien
+                                row.Cells["thanhtien"].Value = soluong * dongia;
+                                // Gọi phương thức để tính tổng số lượng
+                                CalculateTotalQuantity1();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            if (dataGridView1.CurrentCell.OwningColumn.Name == "Column1" && e.Control is ComboBox)
+            {
+                ComboBox comboBox = e.Control as ComboBox;
+                comboBox.SelectedIndexChanged += ComboBox_SelectedIndexChanged;
+            }
+
+        }
+        private void ComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+
+            // Lấy tên của mục đã chọn từ ComboBox
+            string selectedName = GetItemNameByItemID(comboBox.Text);
+            string dvt = GetDVTByItemID(comboBox.Text);
+            // Lấy chỉ số hàng và cột của ô ComboBox
+            int rowIndex = dataGridView1.CurrentCell.RowIndex;
+            int columnIndex = dataGridView1.CurrentCell.ColumnIndex;
+
+            // Gán tên vào Column2 của hàng đã được chọn
+            dataGridView1.Rows[rowIndex].Cells["Column2"].Value = selectedName;
+            dataGridView1.Rows[rowIndex].Cells["Column3"].Value = dvt;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_soluong_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbx_khachhang_SelectedValueChanged(object sender, EventArgs e)
+        {
+            tblCustomerList customerList = data.tblCustomerLists.Where(kh => kh.CustomerID.Equals(cbx_khachhang.Text)).FirstOrDefault();
+            if (customerList != null)
+            {
+                txt_tenkhachhang.Text = customerList.CustomerName;
+                txt_diachi.Text = customerList.Address;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tblOrderMaster newObj = new tblOrderMaster();
+                newObj.OrderDate = Convert.ToDateTime(txt_date.Text);
+                newObj.OrderNo = txt_sohoadon.Text;
+                newObj.CustomerID = cbx_khachhang.Text;
+                newObj.TotalAmount = Convert.ToInt32(txt_soluong.Text);
+                data.tblOrderMasters.InsertOnSubmit(newObj);
+                data.SubmitChanges();
+                int lineNumber = 1; // Biến đếm dòng
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    // Kiểm tra xem hàng có dữ liệu không
+                    if (!row.IsNewRow)
+                    {
+                        // Tạo đối tượng tblOrderDetail cho mỗi hàng
+                        tblOrderDetail newDetail = new tblOrderDetail();
+                        newDetail.OrderID = newObj.OrderID; // Liên kết với OrderNo của tblOrderMaster
+                        newDetail.ItemID = row.Cells["Column1"].Value.ToString(); // Thay "Column1" bằng tên cột thích hợp
+                        newDetail.LineNumber = lineNumber; // Gán giá trị của biến đếm
+                        lineNumber++; // Tăng giá trị biến đếm
+                        newDetail.Quantity = Convert.ToInt32(row.Cells["soluong"].Value); // Thay "soluong" bằng tên cột thích hợp
+                        newDetail.Price = Convert.ToDouble(row.Cells["thanhtien"].Value); // Thay "dongia" bằng tên cột thích hợp
+                        newDetail.Amount = Convert.ToInt32(row.Cells["soluong"].Value); // Thay "thanhtien" bằng tên cột thích hợp
+
+                        // Thêm đối tượng tblOrderDetail vào tblOrderDetails
+                        data.tblOrderDetails.InsertOnSubmit(newDetail);
+                    }
+                }
+                data.SubmitChanges();
+                MessageBox.Show("Thêm thành công");
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            data = new DataClasses1DataContext();
+            Column1.DataSource = data.tblItemLists;
+            Column1.DisplayMember = "ItemID";
+            Column1.ValueMember = "ItemID";
+            dataGridView1.EditingControlShowing += dataGridView1_EditingControlShowing;
+            //
+            cbx_khachhang.DataSource = data.tblCustomerLists;
+            cbx_khachhang.DisplayMember = "CustomerID";
+            cbx_khachhang.ValueMember = "CustomerID";
+            //
+            txt_sohoadon.Text = "";
+            txt_soluong.Text = "";
+            txt_diachi.Text = "";
+            txt_tenkhachhang.Text = "";
+        }
+    }
+}
